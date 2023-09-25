e75701db1cb4c048de620467f174e17a104f4146
Laecio almeida <souazalaecio81@.gmail.com>
2023-09-25 17:33:27 -0300

    projetoPizza

--- Program.cs
+++ Program.cs
@@ -42,7 +42,7 @@ namespace Client
                 //primeira pizza
                 Console.WriteLine($"Nome da pizza: {pizza.NomePizza}");
                 Console.WriteLine($"Sabor: {pizza.Saborpizza}");
-                Console.WriteLine($"Preço: {pizza.PrecoPizza}");
+                Console.WriteLine($"Preço: {pizza.PrecoPizza}$");
 
                  Console.WriteLine("Deseja adcionar outra pizza digite 1/Sim 2/Não.");
                 var opcao3 = double.Parse(Console.ReadLine());
@@ -61,22 +61,24 @@ namespace Client
                 Console.WriteLine("Digite 2 para listar pizza.");
                 var opcao4 = double.Parse(Console.ReadLine());
                 
-            if(opcao3 == 2||opcao4 == 2){
+            if(opcao4 == 2){
 
                 //segunda pizza
                 Console.WriteLine($"Nome da pizza: {pizza2.NomePizza2}");
                 Console.WriteLine($"Sabor: {pizza2.Saborpizza2}");
-                Console.WriteLine($"Preço: {pizza2.PrecoPizza2}");
+                Console.WriteLine($"Preço: {pizza2.PrecoPizza2}$");
 
                  Console.WriteLine("Digite 4 para listar pedido");
                 var opcao5 = double.Parse(Console.ReadLine());
             
 
             
-            if(opcao5 ==4)
+            if(opcao5 ==4 || opcao3 == 2)
             {
                 CadastroPessoa cadas = new CadastroPessoa();
                 var dadosPessoa = cadas.CadastrarUmaPessoa();
+                Console.WriteLine();//impressão vazio para da espaço
+                Console.WriteLine("PEDIDO PRONTO!");
 
                 Console.WriteLine();//impressão vazio para da espaço
                 Console.WriteLine($"Cliente: {dadosPessoa.Nome}");
@@ -85,13 +87,13 @@ namespace Client
                 //primeira pizza
                 Console.WriteLine($"Nome da pizza: {pizza.NomePizza}");
                 Console.WriteLine($"Sabor: {pizza.Saborpizza}");
-                Console.WriteLine($"Preço: {pizza.PrecoPizza}");
+                Console.WriteLine($"Preço: {pizza.PrecoPizza}$");
                 //segunda pizza
-                Console.WriteLine($"Nome da pizza: {pizza2.NomePizza2}");
+                Console.WriteLine($"Nome da segunda pizza: {pizza2.NomePizza2}");
                 Console.WriteLine($"Sabor: {pizza2.Saborpizza2}");
-                Console.WriteLine($"Preço: {pizza2.PrecoPizza2}");
+                Console.WriteLine($"Preço: {pizza2.PrecoPizza2}$");
                 Console.WriteLine();//impressão vazio para da espaço
-                Console.WriteLine($"Valor total: {pizza2.PrecoPizza2 + pizza.PrecoPizza}");
+                Console.WriteLine($"Valor total: {pizza2.PrecoPizza2 + pizza.PrecoPizza}$");
                 Console.WriteLine();//impressão vazio para da espaço
 
                 Console.WriteLine("ESCOLHA A FORMA DE PAGAMENTO: \n 1/Dinheiro. \n2/Cartão. \n3/Vale-refeição. ");
